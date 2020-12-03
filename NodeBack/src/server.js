eadbd75faf2cf3c1e590292fd51f1fdd347b4857
rain2002kr <rain2002kr@gmail.com>
2020-12-03 11:03:51 +0900

    mongo db and mongoose

--- NodeBack/src/server.js
+++ NodeBack/src/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser"); 
+const mongoose = require("mongoose");
+//프로미스 중첩에 빠지지 않도록 도와줌
+mongoose.Promise = global.Promise;
+
 
 const app = express();
 const port = 3000;
@@ -22,7 +26,19 @@ app.get('/test', function (req, res) {
 app.listen(port, ()=>{
     console.log("hi, this is express server")
     console.log(`server started at ${port}`);
+    console.log(`Server backend listening at http://localhost:${port}`);
+    connectDB();
 })
 
-
+function connectDB() {
+    let db;
+    mongoose.connect("mongodb://localhost/test", { useNewUrlParser: true });
+    db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error:"));
+  
+    db.once("open", function () {
+      // we're connected!
+      console.log("Connect to database sucessfully");
+    });
+  }
 
